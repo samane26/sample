d9f04c1c5c8959457c3d386a2ef6a995b1b75c13
samane26 <samane.teymurifar@gmail.com>
2022-02-01 21:32:30 +0330

    Upload sample project on Github

---
+++ sample11.js
@@ -0,0 +1,10 @@
+function  showMenu(){
+    var menu=document.getElementById("menu");
+    if (menu.className=== "header-menu") {
+        menu.className += " show";
+    }
+    else{
+        menu.className += "header-menu";
+
+    }
+}
